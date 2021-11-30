befa8f90520806c45c5161c98f097761feb39dbc
AmineHLub <Amino.bhl@gmail.com>
2021-11-30 18:41:14 +0100

    Remove CREATEDB command, Add data, Add querries

--- schema.sql
+++ schema.sql
@@ -1,9 +1,7 @@
 /* Database schema to keep the structure of entire database. */
-CREATE DATABASE vet_clinic;
-USE vet_clinic;
 
 CREATE TABLE animals (
-    id INT AUTO_INCREMENT NOT NULL,
+    id INT NOT NULL,
     name varchar(100) NOT NULL,
     date_of_birth DATE NOT NULL,
     escape_attempts INT,
