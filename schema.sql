fe0cdd72b30ff70cc64ae2feb8629a56076e5cbf
AmineHLub <Amino.bhl@gmail.com>
2021-11-30 14:12:15 +0100

    CREATE DB, ADD TABLE

--- schema.sql
+++ schema.sql
@@ -1,5 +1,13 @@
 /* Database schema to keep the structure of entire database. */
+CREATE DATABASE vet_clinic;
+USE vet_clinic;
 
 CREATE TABLE animals (
-    name varchar(100)
+    id INT AUTO_INCREMENT NOT NULL,
+    name varchar(100) NOT NULL,
+    date_of_birth DATE NOT NULL,
+    escape_attempts INT,
+    neutered BOOLEAN,
+    weight_kg DECIMAL,
+    PRIMARY KEY (id)
 );
