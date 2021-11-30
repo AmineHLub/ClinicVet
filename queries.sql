c433b4358234dcdafa5905950b3bccee41b02b59
Mohamed Amine Hajltaief <Amino.bhl@gmail.com>
2021-11-30 20:49:00 +0100

    fix minor issue

--- queries.sql
+++ queries.sql
@@ -1,6 +1,6 @@
 /*Queries that provide answers to the questions from all projects.*/
 
-SELECT * FROM animals WHERE name LIKE 'mon%';
+SELECT * FROM animals WHERE name LIKE '%mon';
 SELECT name from animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-01-01';
 SELECT name from animals WHERE neutered=TRUE AND escape_attempts < 3;
 SELECT date_of_birth from animals WHERE name='Agumon' OR name='Pikachu';
