a635ba81704ffbda9de581d65cf4cf296cb8fc1a
AmineHLub <Amino.bhl@gmail.com>
2021-12-01 16:00:15 +0100

    Answer questions in querries.sql

--- queries.sql
+++ queries.sql
@@ -8,3 +8,23 @@ SELECT name, escape_attempts from animals WHERE weight_kg>10.5;
 SELECT name from animals where neutered=TRUE;
 SELECT name from animals where weight_kg BETWEEN 10.4 AND 17.3;
 
+/* HOW MANY IN TOTAL ? */
+SELECT COUNT(name) from animals;
+
+/* HOW MANY TRIED TO ESCAPE */
+SELECT COUNT(name) from animals where escape_attempts > 0;
+
+/* AVERAGE WEIGHT */
+SELECT AVG(weight_kg) from animals;
+
+/* CONDITIONAL SELECTION OF MAX ATTEMPTS ANIMAL NAME*/
+SELECT name from animals
+WHERE escape_attempts = (select MAX(escape_attempts) from animals);
+
+/* WHICH IS MAX/MIN WEIGHT FOR EACH SPECIE */
+SELECT species, MAX(weight_kg), MIN(weight_kg)
+from animals GROUP BY species;
+
+/* AVG ESCAPE ATTEMPT BETWEEN 1990 AND 2000 */
+SELECT species,AVG(escape_attempts) from animals
+where date_of_birth BETWEEN '1990-1-1' AND '2000-1-1' GROUP BY species;
