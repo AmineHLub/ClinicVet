52b35e3a3db9ad315c8d70cc79b9ca88eafa690a
AmineHLub <Amino.bhl@gmail.com>
2021-12-01 14:30:31 +0100

    Add species column to schema.sql and new animals to data

--- data.sql
+++ data.sql
@@ -4,4 +4,11 @@ INSERT INTO animals (id,name,date_of_birth,escape_attempts,neutered,weight_kg)
 VALUES  (1,'Agumon','2020-02-03',0,true,10.23),
         (2,'Gabumon','2018-09-15',2,true,8.00),
         (3,'Pikachu','2021-01-07',1,false,15.04),
-        (4,'Devimon','2017-05-12',5,true,11.00);
+        (4,'Devimon','2017-05-12',5,true,11.00),
+        (5,'Charmander','2022-02-02',0,false,-11.00),
+        (6,'Plantmon','2022-09-15',2,true,-5.70),
+        (7,'Squirtle','1993-04-02',3,false,-12.13),
+        (8,'Angemon','2005-06-12',1,true,-45.00),
+        (9,'Boarmon','2005-06-07',7,true,20.40),
+        (10,'Blossom','1998-10-13',3,true,17.00);
+
