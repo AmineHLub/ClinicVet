8f320ce5fa8db7e6d3fbaa64c96b886fdac522fc
AmineHLub <Amino.bhl@gmail.com>
2021-12-01 15:20:54 +0100

    Add abnormality fixing transactions

--- tran.sql
+++ tran.sql
@@ -22,3 +22,26 @@ COMMIT;
 END;
 
 /* SELECT * FROM animals;  ===> would show all new updated data */
+
+/* FAKE DELETE */
+BEGIN;
+DELETE from animals *;
+SELECT * from animals; /* WOULD SHOW EMPTY DATABASE */
+ROLLBACK;
+END;
+
+SELECT * from animals; /* ALL THE DATA IS THERE */
+
+/* ABNORMALITY FIXING (negative weight/ future born) */
+BEGIN;
+DELETE from animals where date_of_birth >= '2022-01-01';
+COMMIT;
+UPDATE animals
+SET weight_kg = weight_kg*-1;
+/* SELECT * from animals; ===> ALL weights are opposit to what they were */
+ROLLBACK;
+UPDATE animals
+SET weight_kg = weight_kg*-1 where weight_kg<0;
+COMMIT;
+/* SELECT * from animals; ===> ALL weights positive */
+END;
