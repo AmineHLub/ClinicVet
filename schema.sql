5c00c6957d990da1acbe758333a2113432035038
AmineHLub <Amino.bhl@gmail.com>
2021-12-02 11:52:02 +0100

    Minor refactoring primary key

--- schema.sql
+++ schema.sql
@@ -1,13 +1,13 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-    id INT NOT NULL,
+    id SERIAL NOT NULL,
     name varchar(100) NOT NULL,
     date_of_birth DATE NOT NULL,
     escape_attempts INT,
     neutered BOOLEAN,
     weight_kg DECIMAL,
-    species varchar(100) NOT NULL ,
+    species varchar(100),
     PRIMARY KEY (id)
 );
 
