8d6c91e26457b7f92dcdab75b8efaaf2729f421b
Mohamed Amine Hajltaief <Amino.bhl@gmail.com>
2021-12-02 19:12:22 +0100

    Copy queries from data.sql

--- queries.sql
+++ queries.sql
@@ -28,3 +28,46 @@ from animals GROUP BY species;
 /* AVG ESCAPE ATTEMPT BETWEEN 1990 AND 2000 */
 SELECT species,AVG(escape_attempts) from animals
 where date_of_birth BETWEEN '1990-1-1' AND '2000-1-1' GROUP BY species;
+
+/* animals that belong to melody pond  */
+
+SELECT name FROM animals JOIN owners ON owners.id=animals.owners_id
+WHERE full_name = 'Melody Pond';
+
+/* animals of type pokemon */
+
+select animals.name from animals
+JOIN species on animals.species_id = species.id
+WHERE species.name = 'Pokemon';
+
+/* animals and their owners  */
+
+SELECT owners.full_name, animals.name FROM owners
+LEFT JOIN animals ON owners.id = animals.owners_id;
+
+/* species count */
+
+SELECT species.name, count( animals.species_id ) FROM species
+join animals on animals.species_id = species.id
+GROUP BY species.name;
+
+/* all pokemons jennifer has */
+
+SELECT animals.name FROM species
+join animals on animals.species_id = species.id
+JOIN owners ON owners.id = animals.owners_id
+where owners.full_name='Jennifer Orwell' AND species.name='Pokemon';
+
+/* Animals that belongs to deaen and haven't tried to escape */
+
+SELECT name FROM animals
+JOIN owners ON owners.id = animals.owners_id
+WHERE owners.full_name='Dean Winchester' AND  animals.escape_attempts=0;
+
+/* who has most animals */
+
+SELECT owners.full_name FROM animals
+JOIN owners ON owners.id = animals.owners_id
+GROUP BY owners.full_name
+ORDER BY count(name) desc
+limit 1;
